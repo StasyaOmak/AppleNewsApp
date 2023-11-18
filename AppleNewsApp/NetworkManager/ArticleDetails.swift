dc0be3dacf634b89b556ccf0cc30850c7e848d61
Anastasiya <ios.developer.by@gmail.com>
2023-11-18 22:34:12 +0200

    Class Work

---
+++ AppleNewsApp/NetworkManager/ArticleDetails.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleDetails.swift
+//  AppleNewsApp
+//
+//  Created by Anastasiya Omak on 18/11/2023.
+//
+
+import UIKit
+
+class ArticleDetails: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+}
